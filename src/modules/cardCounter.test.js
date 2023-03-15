3c741e2a7a3da22b49712d4d983a0af1f499f462
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-03-15 14:48:35 -0600

    Add test to card counter

---
+++ src/modules/cardCounter.test.js
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import cardCounter from "./cardCounter";
+
+test('Test that the amount of card is correct', () => {
+  document.body.innerHTML =
+    '<div class="card">' +
+    '</div>' + 
+    '<div class="card">' +
+    '</div>';
+
+  expect(cardCounter()).toEqual(2);
+});
