bbc1e7b2595f21fefd16400fe94b7ce943fe3d64
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-03-15 14:58:34 -0600

    Fix eslint errors

--- src/modules/cardCounter.test.js
+++ src/modules/cardCounter.test.js
@@ -2,14 +2,13 @@
  * @jest-environment jsdom
  */
 
-import cardCounter from "./cardCounter";
+import cardCounter from './cardCounter.js';
 
 test('Test that the amount of card is correct', () => {
-  document.body.innerHTML =
-    '<div class="card">' +
-    '</div>' + 
-    '<div class="card">' +
-    '</div>';
+  document.body.innerHTML = '<div class="card">'
+    + '</div>'
+    + '<div class="card">'
+    + '</div>';
 
   expect(cardCounter()).toEqual(2);
 });
